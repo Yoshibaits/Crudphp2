00f805b00345f597f4b9befd92acc8359bd14f2a
Yoshibaits <neil_dsg@yahoo.com>
2024-02-07 10:11:17 +0800

    center the login form in index.php

--- login/index.php
+++ login/index.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="./style.css">
     </head>
-  <body class="overflow-hidden">
+  <body class="overflow-hidden d-flex justify-content-center align-items-center">
     <div class="container my-5 d-flex justify-content-center align-items-center">
         <form action="login.php" method="post" class="mb-auto mx-auto">
             <h4 class="text-center">Login</h4>
